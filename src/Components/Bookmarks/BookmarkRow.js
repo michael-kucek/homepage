6742080b0dc657193235822688ccb27b35165358
Michael Kucek <3052430+michael-kucek@users.noreply.github.com>
2022-11-16 19:05:08 -0600

    fix for bookmark click

--- src/Components/Bookmarks/BookmarkRow.js
+++ src/Components/Bookmarks/BookmarkRow.js
@@ -2,10 +2,8 @@ import './BookmarkStyles.css'
 
 export default function BookmarkRow({ url, name, color, background }) {
     return (
-        <div className='bookmark-div shadow' style={{ background }}>
-            <a style={{ color }} className='bookmark-anchor' href={url} target='_blank' rel="noreferrer">
-                {name}
+            <a style={{ color, background }} className='bookmark-anchor bookmark-div shadow' href={url} target='_blank' rel="noreferrer">
+                <span>{name}</span>
             </a>
-        </div>
     )
 }
